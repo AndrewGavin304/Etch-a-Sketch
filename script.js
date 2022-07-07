18e7470831f8f6dbb16c1df076254dc158918cd2
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-07 18:38:28 -0400

    Finished

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 //create flex container with margin auto, align-items center, justify-content center
-function createContainer(rows, columns) {
-const width = rows * 30;
-const height = columns * 30
+function createContainer() {
+const width = '800px';
+const height = '800px';
 const newDiv = document.createElement("div")
 newDiv.style.cssText = "display:flex; flex-wrap: wrap; margin:auto; align-items: center; justify-content: center; padding:0px; background-color: gray"
 newDiv.setAttribute("id", "gridContainer")
-newDiv.style.width = width+"px";
-newDiv.style.height = height+"px";
+newDiv.style.width = width;
+newDiv.style.height = height;
 document.body.appendChild(newDiv)
 }
 
 //create create function that creates 16 flex column 'topDiv's with specified width/height, 0margin, 0padding
 
-function createGrid(divs) {
-    for (let i = 0; i < divs; i++) {
+function createGrid(divs, rows, columns) {
+    for (let i = 0; i < (divs); i++) {
         const newDiv = document.createElement("div")
         newDiv.style.cssText = "display: flex; margin: 0px; padding: 0px"
         newDiv.setAttribute("class", "box")
-        newDiv.style.width = "30px";
-        newDiv.style.height = "30px";
+        newDiv.style.width = `${800/columns}px`
+        newDiv.style.height = `${800/rows}px`
         document.getElementById("gridContainer").appendChild(newDiv)
     }
 }
 
+function customGrid() {
 let rows = prompt("How many rows do you want in your etch-a-sketch?", "16")
 let columns = prompt("How many columns do you want in your etch-a-sketch?", "16")
-let area = rows * columns
+let divs = rows * columns
+clearPad()
+createGrid(divs, rows, columns)
+enableSketch()
+}
 
-createContainer(rows, columns)
-createGrid(area)
-
-const boxes = document.querySelectorAll(".box")
-
-boxes.forEach( box =>
+function enableSketch() {
+    const boxes = document.querySelectorAll(".box")
+    boxes.forEach( box =>
     box.addEventListener("mouseover", function (e) {
         e.target.setAttribute("class", "hovered")
     })
 )
+}
+
+function clearPad() {
+    const boxes = document.querySelectorAll(".box")
+    const hovered = document.querySelectorAll(".hovered")
+    boxes.forEach( box => box.remove())
+    hovered.forEach( hovered => hovered.remove())
+}
+
+createContainer()
+createGrid(256, 16, 16)
+enableSketch()
+
+
+document.getElementById("custom").addEventListener("click", customGrid);
