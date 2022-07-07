67b1bdf7a7ef6a04d9f245c6bf88072354ab3a54
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-07 15:56:32 -0400

    Basic pseudocode, load js script

---
+++ script.js
@@ -0,0 +1,5 @@
+//create flex container with margin auto, align-items center, justify-content center
+
+//create create function that creates 16 flex column 'topDiv's with specified width/height, 0margin, 0padding
+
+//for each topDiv, append 15 child divs 
